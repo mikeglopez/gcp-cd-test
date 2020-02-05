88ef22e3be43ea4584f0829c31bac670fc0b3467
Mike <michael.lopez@flowersfordreams.com>
2020-02-05 14:11:52 -0600

    Passing tests

--- app.test.js
+++ app.test.js
@@ -2,12 +2,20 @@ const request = require('supertest');
 const app = require('./app')
 
 describe('App', () => {
-    test('should return 200', async () => {
+    // test('should return 200', async () => {
+    //     const response = await request(app).get('/');
+    //     expect(response.statusCode).toBe(200);
+    // });
+    // test('should return Hello World!', async () => {
+    //     const response = await request(app).get('/');
+    //     expect(response.text).toBe('Hello World!');
+    // });
+    test('should return 418', async () => {
         const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(418);
     });
-    test('should return Hello World!', async () => {
+    test("should return I'm a teapot", async () => {
         const response = await request(app).get('/');
-        expect(response.text).toBe('Hello World!');
+        expect(response.text).toBe("I'm a teapot");
     });
 })
